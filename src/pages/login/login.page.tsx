2b1921a6a8e52f6ff327de3f7e3700900bb5b716
David Lima - A.k.a. Xarlyu <47418013+DavidProf@users.noreply.github.com>
2022-08-02 17:31:55 -0300

    Initial commit - react native project to define folder structure

---
+++ src/pages/login/login.page.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import { View } from 'react-native'
+import { loginStyles } from './login.styles'
+
+export const Feed = () => {
+    return <View style={loginStyles.view} />
+}
